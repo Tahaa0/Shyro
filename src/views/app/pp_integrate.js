5926b0f3e956b68708df9fc92b1a534f1caf64e2
Taha <dominor001@gmail.com>
2020-09-02 16:05:21 +0100

    e

--- src/views/app/pp_integrate.js
+++ src/views/app/pp_integrate.js
@@ -1,9 +1,6 @@
 window.onload = function(){
 	var pathParts = window.location.pathname.split('/');
 
-	var END_URL = "order-formpptest";
-	var PAYPAL_URL = "4o18y3tndu27pfer";
-
 	if(pathParts[pathParts.length-1] == END_URL){
 		
 		var styleCode= ".a_tab{\r\n        border: 1px solid #E0E0E0;\r\n        width: 100%;\r\n        padding: 10px;\r\n        font-family: \'Lato\';\r\n        cursor:pointer;\r\n    }\r\n    .a_body{\r\n        width: 100%;\r\n        height: 80px;\r\n        border-right: 1px solid #E0E0E0;\r\n        border-left: 1px solid #E0E0E0;\r\n        padding: 10px;\r\n        background-color: #F8F8F9;\r\n        display:none;\r\n    }\r\n    .ttl{\r\n        font-weight: 500;\r\n    }\r\n    #paypalTab{\r\n        border-radius: 0 0 5px 5px;\r\n        box-shadow: 0px 1px 1px #E0E0E0;\r\n        margin-top: -1px;\r\n    }  \r\n    #ccTab{\r\n        border-radius: 5px 5px 0px 0px;\r\n        padding-bottom: 11px;\r\n    }\r\n    #paypalBody{\r\n        border-radius: 0 0 5px 5px;\r\n        box-shadow: 0px 1px 1px #E0E0E0;\r\n    }\r\n  \t#ccBody{\r\n    \tdisplay:block;\r\n    }\r\n    .nobottom{\r\n        border-radius: 0 !important;\r\n        box-shadow: 0 !important;\r\n    }\r\n    .a_p{\r\n        color:#737376;\r\n        font-family: \'Lato\';\r\n        text-align: center; \r\n    }\r\n  \ta[href=\'#submit-form\'], a[href=\'#submit-form-2step-order\']{\r\n      margin-top:10px;\r\n    }\r\n";
@@ -11,7 +8,7 @@ window.onload = function(){
 		styleElement.innerHTML = styleCode;
 		var head = document.getElementsByTagName('head')[0];
 		head.appendChild(styleElement);
-		
+
 		$('.o2step_step2 .elCreditCardForm').remove();
         window.buyButton = $("a[href='#submit-form'], a[href='#submit-form-2step-order']");
         var $parentDiv = window.buyButton.parent();
