457cd7058008fd62a4bf76f17f5cb909a67b004e
Taha <dominor001@gmail.com>
2020-09-09 12:13:15 +0100

    e

--- src/views/app/pp_body.js
+++ src/views/app/pp_body.js
@@ -17,7 +17,7 @@ for(var i=0;i<STEP.products.length;i++){
   var PARENT_ = $('#pid-'+STEP.products[i].paypal+'-0').parent().parent()
   console.log(PARENT_);
   PARENT_.remove();
-  $('#pid-'+STEP.products[i].stripe+'-0').attr('id','xx-'+STEP.products[i].stripe);
+  $('#pid-'+STEP.products[i].stripe+'-0').attr('name','xxprod');
 }
 
 var cForm = $('.elCreditCard');
