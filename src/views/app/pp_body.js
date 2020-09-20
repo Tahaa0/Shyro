0498d953e039f7180158d8a457857b51723fa755
Taha <dominor001@gmail.com>
2020-09-20 12:19:37 +0100

    nigacat'

--- src/views/app/pp_body.js
+++ src/views/app/pp_body.js
@@ -1,4 +1,4 @@
-var pathParts = window.location.pathname.split('/');
+  var pathParts = window.location.pathname.split('/');
   var endPath = pathParts[pathParts.length-1].trim();
   var VALID = false;
   var STEP;
@@ -9,36 +9,41 @@ var pathParts = window.location.pathname.split('/');
       break;
     }
   }
-  if(VALID){
 
-console.log(STEP);
+if(VALID){
 
-//$('#bump-offer').attr('name','xxprod');
-for(var i=0;i<STEP.products.length;i++){
-  var PARENT_ = $('#pid-'+STEP.products[i].paypal+'-0').parent().parent()
-  if(STEP.products[i].bump){
-    $('#pid-'+STEP.products[i].stripe+'-0').parent().parent().remove();
-  }
-  PARENT_.remove();
-  $('#pid-'+STEP.products[i].stripe+'-0').attr('name','xxprod');
-}
+  console.log(STEP);
+  if(STEP.type = 'form'){
 
 
+    //change name and remove paypal products from checkout (form)
+    for(var i=0;i<STEP.products.length;i++){
+      var PARENT_ = $('#pid-'+STEP.products[i].paypal+'-0').parent().parent()
+      if(STEP.products[i].bump){
+        $('#pid-'+STEP.products[i].stripe+'-0').parent().parent().remove();
+      }
+      PARENT_.remove();
+      $('#pid-'+STEP.products[i].stripe+'-0').attr('name','xxprod');
+    }
 
-var cForm = $('.elCreditCard');
-    var bButton = $("a[href='#submit-form'], a[href='#submit-form-2step-order']");
-    var formClone = cForm.clone();
+
+    //change button and credit card form
+    var cForm = $('.elCreditCard');
+        var bButton = $("a[href='#submit-form'], a[href='#submit-form-2step-order']");
+        var formClone = cForm.clone();
+        
+        var cPrevious = cForm.prev();
+
+         $(function(){
+            console.log(cPrevious);
+            console.log(formClone);
+          cPrevious.after(formClone);
+            bButton.show();
+         });
     
-    var cPrevious = cForm.prev();
-
-     $(function(){
-        console.log(cPrevious);
-        console.log(formClone);
-      cPrevious.after(formClone);
-        bButton.show();
-     });
-  
-
+  }else if(STEP.type = 'upsell'){
+    console.log('upselll');
+  }
     
   
 
--- src/views/app/pp_head.js
+++ src/views/app/pp_head.js
@@ -2,20 +2,14 @@ window.onload = function(){
 
 	var CHOSEN_ID = $('#cfAR input[name="purchase[product_id]"]:first').val();
   var BUMPS = [];
-  /*var BUMP = false;
-  var BUMP_PRODUCT;
-  for(var i=0;i<STEP.products.length;i++){
-    if(STEP.products[i].bump){
-      BUMP_PRODUCT = STEP.products[i];        
-    }
-  }*/
+  
   var HOST = "http://cfapp20.herokuapp.com";
   var $form = $('#cfAR');
-    var FORM = $("<form></form>");
-    FORM.attr('target',$form.attr('target'));
-    FORM.attr('method',$form.attr('method'));
-    FORM.attr('action',$form.attr('action'));
-    FORM.html(serializeToForm($form.serializeArray()));
+  var FORM = $("<form></form>");
+  FORM.attr('target',$form.attr('target'));
+  FORM.attr('method',$form.attr('method'));
+  FORM.attr('action',$form.attr('action'));
+  FORM.html(serializeToForm($form.serializeArray()));
 
   function calculateID() {
     for(var i=0;i<STEP.products.length;i++){
@@ -78,92 +72,93 @@ window.onload = function(){
   }
 	if(VALID){
 		
-    //ADD STYLE
-    var styleCode= ".a_tab{\r\n        border: 1px solid #E0E0E0;\r\n        width: 100%;\r\n        padding: 10px;\r\n        font-family: \'Lato\';\r\n        cursor:pointer;\r\n    }\r\n    .a_body{\r\n        width: 100%;\r\n        height: auto;\r\n        border-right: 1px solid #E0E0E0;\r\n        border-left: 1px solid #E0E0E0;\r\n        padding: 10px;\r\n        background-color: #F8F8F9;\r\n        display:none;\r\n    }\r\n    .ttl{\r\n        font-weight: 500;\r\n    }\r\n    #paypalTab{\r\n        border-radius: 0 0 5px 5px;\r\n        box-shadow: 0px 1px 1px #E0E0E0;\r\n        margin-top: -1px;\r\n    }  \r\n    #ccTab{\r\n        border-radius: 5px 5px 0px 0px;\r\n        padding-bottom: 11px;\r\n    }\r\n    #paypalBody{\r\n        border-radius: 0 0 5px 5px;\r\n        box-shadow: 0px 1px 1px #E0E0E0;\r\n    }\r\n  \t#ccBody{\r\n    \tdisplay:block;\r\n    }\r\n    .nobottom{\r\n        border-radius: 0 !important;\r\n        box-shadow: 0 !important;\r\n    }\r\n    .a_p{\r\n        color:#737376;\r\n        font-family: \'Lato\';\r\n        text-align: center; \r\n    }\r\n  \ta[href=\'#submit-form\'], a[href=\'#submit-form-2step-order\']{\r\n      margin-top:10px;\r\n    }\r\n";
-    var styleElement = document.createElement('style');
-    var customStyle = "\r\n.ccStripeElementCard {\r\n    float: none;\r\n} #log_pp{height:28px} #log_cc{height:21px; float:right; padding-right: 4px;}";
-    var radioStyle = "\/* The container *\/\r\n.radlab {\r\n  position: relative;\r\n  padding-left: 35px;\r\n  margin-bottom: 12px;\r\n  cursor: pointer;\r\n  font-size: 22px;\r\n  -webkit-user-select: none;\r\n  -moz-user-select: none;\r\n  -ms-user-select: none;\r\n  user-select: none;\r\n}\r\n\r\n\/* Hide the browser\'s default radio button *\/\r\n.radlab input {\r\n  position: absolute;\r\n  opacity: 0;\r\n  cursor: pointer;\r\n}\r\n\r\n\/* Create a custom radio button *\/\r\n.checkmark {\r\n  position: absolute;\r\n  top: -2px;\r\n  left: 0;\r\n  height: 18px;\r\n  width: 18px;\r\n  background-color: #eee;\r\n  border-radius: 50%;\r\n}\r\n\r\n\/* On mouse-over, add a grey background color *\/\r\n.radlab:hover input ~ .checkmark {\r\n  background-color: #ccc;\r\n}\r\n\r\n\/* When the radio button is checked, add a blue background *\/\r\n.radlab input:checked ~ .checkmark {\r\n  background-color: #2196F3;\r\n}\r\n\r\n\/* Create the indicator (the dot\/circle - hidden when not checked) *\/\r\n.checkmark:after {\r\n  content: \"\";\r\n  position: absolute;\r\n  display: none;\r\n}\r\n\r\n\/* Show the indicator (dot\/circle) when checked *\/\r\n.radlab input:checked ~ .checkmark:after {\r\n  display: block;\r\n}\r\n\r\n\/* Style the indicator (dot\/circle) *\/\r\n.radlab .checkmark:after {\r\n \ttop: 5px;\r\n\tleft: 5px;\r\n\twidth: 8px;\r\n\theight: 8px;\r\n\tborder-radius: 50%;\r\n\tbackground: white;\r\n}";
-    styleElement.innerHTML = styleCode+customStyle+radioStyle;
-    var head = document.getElementsByTagName('head')[0];
-    head.appendChild(styleElement);
-    
-    //-----------------
-    //ADD FORM / BUTTON
-    checkProd(calculateID());
-
-
-
-    window.buyButton = $("a[href='#submit-form'], a[href='#submit-form-2step-order']");
-    var $parentDiv = window.buyButton.parent();
+    if(STEP.type = 'form'){
+      //ADD STYLE
+      var styleCode= ".a_tab{\r\n        border: 1px solid #E0E0E0;\r\n        width: 100%;\r\n        padding: 10px;\r\n        font-family: \'Lato\';\r\n        cursor:pointer;\r\n    }\r\n    .a_body{\r\n        width: 100%;\r\n        height: auto;\r\n        border-right: 1px solid #E0E0E0;\r\n        border-left: 1px solid #E0E0E0;\r\n        padding: 10px;\r\n        background-color: #F8F8F9;\r\n        display:none;\r\n    }\r\n    .ttl{\r\n        font-weight: 500;\r\n    }\r\n    #paypalTab{\r\n        border-radius: 0 0 5px 5px;\r\n        box-shadow: 0px 1px 1px #E0E0E0;\r\n        margin-top: -1px;\r\n    }  \r\n    #ccTab{\r\n        border-radius: 5px 5px 0px 0px;\r\n        padding-bottom: 11px;\r\n    }\r\n    #paypalBody{\r\n        border-radius: 0 0 5px 5px;\r\n        box-shadow: 0px 1px 1px #E0E0E0;\r\n    }\r\n  \t#ccBody{\r\n    \tdisplay:block;\r\n    }\r\n    .nobottom{\r\n        border-radius: 0 !important;\r\n        box-shadow: 0 !important;\r\n    }\r\n    .a_p{\r\n        color:#737376;\r\n        font-family: \'Lato\';\r\n        text-align: center; \r\n    }\r\n  \ta[href=\'#submit-form\'], a[href=\'#submit-form-2step-order\']{\r\n      margin-top:10px;\r\n    }\r\n";
+      var styleElement = document.createElement('style');
+      var customStyle = "\r\n.ccStripeElementCard {\r\n    float: none;\r\n} #log_pp{height:28px} #log_cc{height:21px; float:right; padding-right: 4px;}";
+      var radioStyle = "\/* The container *\/\r\n.radlab {\r\n  position: relative;\r\n  padding-left: 35px;\r\n  margin-bottom: 12px;\r\n  cursor: pointer;\r\n  font-size: 22px;\r\n  -webkit-user-select: none;\r\n  -moz-user-select: none;\r\n  -ms-user-select: none;\r\n  user-select: none;\r\n}\r\n\r\n\/* Hide the browser\'s default radio button *\/\r\n.radlab input {\r\n  position: absolute;\r\n  opacity: 0;\r\n  cursor: pointer;\r\n}\r\n\r\n\/* Create a custom radio button *\/\r\n.checkmark {\r\n  position: absolute;\r\n  top: -2px;\r\n  left: 0;\r\n  height: 18px;\r\n  width: 18px;\r\n  background-color: #eee;\r\n  border-radius: 50%;\r\n}\r\n\r\n\/* On mouse-over, add a grey background color *\/\r\n.radlab:hover input ~ .checkmark {\r\n  background-color: #ccc;\r\n}\r\n\r\n\/* When the radio button is checked, add a blue background *\/\r\n.radlab input:checked ~ .checkmark {\r\n  background-color: #2196F3;\r\n}\r\n\r\n\/* Create the indicator (the dot\/circle - hidden when not checked) *\/\r\n.checkmark:after {\r\n  content: \"\";\r\n  position: absolute;\r\n  display: none;\r\n}\r\n\r\n\/* Show the indicator (dot\/circle) when checked *\/\r\n.radlab input:checked ~ .checkmark:after {\r\n  display: block;\r\n}\r\n\r\n\/* Style the indicator (dot\/circle) *\/\r\n.radlab .checkmark:after {\r\n \ttop: 5px;\r\n\tleft: 5px;\r\n\twidth: 8px;\r\n\theight: 8px;\r\n\tborder-radius: 50%;\r\n\tbackground: white;\r\n}";
+      styleElement.innerHTML = styleCode+customStyle+radioStyle;
+      var head = document.getElementsByTagName('head')[0];
+      head.appendChild(styleElement);
       
-    var anc = $parentDiv.parent();
-    anc.append(window.buyButton);
-    $parentDiv.hide();
-  
-    var ccForm = $('.elCreditCardForm');
-    var parDOM = ccForm.parent();
-    console.log(parDOM);
-    var ccTab = $('<div id="ccTab" class="a_tab"><label class="radlab"><input type="radio" checked="checked" name="ispp" id="ccRadio"><span class="checkmark"></span></label><span class="ttl">Credit Card</span><img id="log_cc" src="'+HOST+'/views/app/creditcards.png"></div>');
-    var ccBody = $('<div id="ccBody" class="a_body"></div>');
-    var ppTab = $('<div id="paypalTab" class="a_tab"><label class="radlab"><input type="radio" name="ispp" id="ppRadio"><span class="checkmark"></span></label><span class="ttl"><img id="log_pp" src="'+HOST+'/views/app/paypal.png"></span></div>');
-    var ppBody = $('<div id="paypalBody" class="a_body"><p class="a_p">After submitting, you will be redirected to paypal to purchase securely.</p></div>');
-    
-    ccTab.on('click',function(){
-      isPAYPAL = false;
-      $('#ccRadio').attr('checked', 'checked');
-      ppBody.slideUp(200);
-      ccBody.slideDown(200);
-      ppTab.removeClass('nobottom');
-      window.buyButton.show();
+      //-----------------
+      //ADD FORM / BUTTON
+      checkProd(calculateID());
+
+
+
+      window.buyButton = $("a[href='#submit-form'], a[href='#submit-form-2step-order']");
+      var $parentDiv = window.buyButton.parent();
+        
+      var anc = $parentDiv.parent();
+      anc.append(window.buyButton);
       $parentDiv.hide();
-      checkProd(calculateID());
-    });
-    ppTab.click(function(){
-      isPAYPAL = true;
-      $('#ppRadio').attr('checked', 'checked');
-      ccBody.slideUp(200);
-      ppBody.slideDown(200);
-      ppTab.addClass('nobottom');
-      window.buyButton.hide();
-      $parentDiv.show();
-      checkProd(calculateID());
-    });
-
-    $('input[name=xxprod]').click(function () {
-      CHOSEN_ID = $('input[name=xxprod]:checked').val();
-      checkProd(calculateID());
-    });
     
-    $('input[name=xxprod]:first').attr('checked','checked');
-    CHOSEN_ID = $('input[name=xxprod]:first').val();
-    checkProd(calculateID());
-
-    $('input[name=xxbump]').click(function (){
-      BUMPS = [];
-      $('input[name=xxbump]').each(function(index){
-        if($(this).is(':checked')){
-          BUMPS.push($(this).val());
-        }
+      var ccForm = $('.elCreditCardForm');
+      var parDOM = ccForm.parent();
+      console.log(parDOM);
+      var ccTab = $('<div id="ccTab" class="a_tab"><label class="radlab"><input type="radio" checked="checked" name="ispp" id="ccRadio"><span class="checkmark"></span></label><span class="ttl">Credit Card</span><img id="log_cc" src="'+HOST+'/views/app/creditcards.png"></div>');
+      var ccBody = $('<div id="ccBody" class="a_body"></div>');
+      var ppTab = $('<div id="paypalTab" class="a_tab"><label class="radlab"><input type="radio" name="ispp" id="ppRadio"><span class="checkmark"></span></label><span class="ttl"><img id="log_pp" src="'+HOST+'/views/app/paypal.png"></span></div>');
+      var ppBody = $('<div id="paypalBody" class="a_body"><p class="a_p">After submitting, you will be redirected to paypal to purchase securely.</p></div>');
+      
+      ccTab.on('click',function(){
+        isPAYPAL = false;
+        $('#ccRadio').attr('checked', 'checked');
+        ppBody.slideUp(200);
+        ccBody.slideDown(200);
+        ppTab.removeClass('nobottom');
+        window.buyButton.show();
+        $parentDiv.hide();
+        checkProd(calculateID());
       });
-      checkProd(calculateID());
-    });
+      ppTab.click(function(){
+        isPAYPAL = true;
+        $('#ppRadio').attr('checked', 'checked');
+        ccBody.slideUp(200);
+        ppBody.slideDown(200);
+        ppTab.addClass('nobottom');
+        window.buyButton.hide();
+        $parentDiv.show();
+        checkProd(calculateID());
+      });
 
-    parDOM.prepend(ppBody);
-    parDOM.prepend(ppTab);
-    parDOM.prepend(ccBody);
-    parDOM.prepend(ccTab);
-    ccBody.prepend(ccForm);
-    
-    //---------------------------
-    $('#cfAR').on('submit',function(e){
-      e.preventDefault();
-      FORM.html(serializeToForm($form.serializeArray()));
-      $('body').prepend(FORM);
-      FORM.submit();
-    });
+      $('input[name=xxprod]').click(function () {
+        CHOSEN_ID = $('input[name=xxprod]:checked').val();
+        checkProd(calculateID());
+      });
+      
+      $('input[name=xxprod]:first').attr('checked','checked');
+      CHOSEN_ID = $('input[name=xxprod]:first').val();
+      checkProd(calculateID());
+
+      $('input[name=xxbump]').click(function (){
+        BUMPS = [];
+        $('input[name=xxbump]').each(function(index){
+          if($(this).is(':checked')){
+            BUMPS.push($(this).val());
+          }
+        });
+        checkProd(calculateID());
+      });
+
+      parDOM.prepend(ppBody);
+      parDOM.prepend(ppTab);
+      parDOM.prepend(ccBody);
+      parDOM.prepend(ccTab);
+      ccBody.prepend(ccForm);
+      
+      //---------------------------
+      $('#cfAR').on('submit',function(e){
+        e.preventDefault();
+        FORM.html(serializeToForm($form.serializeArray()));
+        $('body').prepend(FORM);
+        FORM.submit();
+      });
+    }
+  }else if(STEP.type = 'upsell'){
+    console.log('upsell!1!!');
   }
-  //CREDIT CARD
-  
-  
   
 }
