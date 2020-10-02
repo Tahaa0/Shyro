bd50f34742a60b1c351d160774a0769a800146e3
Taha <dominor001@gmail.com>
2020-10-02 15:17:05 +0100

    ee

--- src/views/app/pp_body.js
+++ src/views/app/pp_body.js
@@ -1,3 +1,24 @@
+  function getTimeNow(){
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var d = new Date();
+    var day = days[d.getDay()];
+    var hr = d.getHours();
+    var min = d.getMinutes();
+    if (min < 10) {
+        min = "0" + min;
+    }
+    var ampm = "am";
+    if( hr > 12 ) {
+        hr -= 12;
+        ampm = "pm";
+    }
+    var date = d.getDate();
+    var month = months[d.getMonth()];
+    var year = d.getFullYear();
+    return hr + ":" + min + ampm + " " + date + " " + month + " " + year;
+  }
+
   var pathParts = window.location.pathname.split('/');
   var endPath = pathParts[pathParts.length-1].trim();
   var VALID = false;
@@ -24,6 +45,16 @@ if(VALID){
       }
       PARENT_.remove();
       $('#pid-'+STEP.products[i].stripe+'-0').attr('name','xxprod');
+      if(STEP.products[i].variants){
+        if(STEP.products[i].variants.length > 0){
+          for(var j=0;j<STEP.products[i].variants.length;j++){
+            var HTML = $('#cfAR').html();
+            var _name = STEP.products[i].title + '_' + STEP.products[i].variants[j].title + '_' + getTimeNow();
+            HTML += '<input type="hidden" id="xxvar'+i+'_'+j+'" name="'+_name+'" value="">';
+            $('#cfAR').html(HTML);
+          }
+        }
+      }
     }
 
 
